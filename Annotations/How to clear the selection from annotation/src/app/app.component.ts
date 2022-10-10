ce698632119e3ad10f4cf0694f0ccd11116a6b10
RanjithaAmirthalingam <63278452+RanjithaAmirthalingam@users.noreply.github.com>
2022-10-10 11:00:31 +0530

    BLAZ-25693 Angular samples were added and committed.

---
+++ Annotations/How to clear the selection from annotation/src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import {
+  PdfViewerComponent,
+  LinkAnnotationService,
+  BookmarkViewService,
+  MagnificationService,
+  ThumbnailViewService,
+  ToolbarService,
+  NavigationService,
+  TextSearchService,
+  TextSelectionService,
+  PrintService,
+  AnnotationService,
+  FormFieldsService,
+} from '@syncfusion/ej2-angular-pdfviewer';
+
+/**
+ * Default PdfViewer Controller
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  encapsulation: ViewEncapsulation.None,
+  providers: [
+    LinkAnnotationService,
+    BookmarkViewService,
+    MagnificationService,
+    ThumbnailViewService,
+    ToolbarService,
+    NavigationService,
+    TextSearchService,
+    TextSelectionService,
+    PrintService,
+    AnnotationService,
+    FormFieldsService,
+  ],
+})
+export class AppComponent {
+  public service: string =
+    'https://ej2services.syncfusion.com/production/web-services/api/pdfviewer';
+  public document: string = 'PDF_Succinctly.pdf';
+  ngOnInit(): void {
+  }
+  //Method to add free text annotation programmatically.
+  AddFreeText() {
+    var pdfviewer = (<any>document.getElementById('pdfViewer'))
+      .ej2_instances[0];
+    pdfviewer.freeTextSettings.width = 200;
+    pdfviewer.freeTextSettings.height = 50;
+    pdfviewer.freeTextSettings.textAlignment = 'Center';
+    pdfviewer.freeTextSettings.borderStyle = 'solid';
+    pdfviewer.freeTextSettings.borderWidth = 2;
+    pdfviewer.freeTextSettings.borderColor = 'blue';
+    pdfviewer.freeTextSettings.fillColor = 'blue';
+    pdfviewer.freeTextSettings.fontSize = 14;
+    pdfviewer.freeTextSettings.fontColor = 'black';
+    pdfviewer.freeTextSettings.defaultText = 'Dinuka';
+    pdfviewer.freeTextSettings.isReadonly = true;
+    pdfviewer.annotationModule.setAnnotationMode('FreeText');
+  }
+  //Method to remove the selection from free text annotation.
+  RemoveFreeText() {
+    var pdfviewer = (<any>document.getElementById('pdfViewer'))
+      .ej2_instances[0];
+    pdfviewer.annotationModule.setAnnotationMode('Circle');
+  }
+}
---
+++ Save and Load/Load PDF with WebService/Load the PDF as blob/src/app/app.component.ts
@@ -0,0 +1,70 @@
+
+import { Component, OnInit } from '@angular/core';
+import {
+  PdfViewerComponent,
+  LinkAnnotationService, 
+  BookmarkViewService,
+  MagnificationService, 
+  ThumbnailViewService,
+  ToolbarService, 
+  NavigationService, 
+  TextSearchService, 
+  TextSelectionService, 
+  PrintService
+} from '@syncfusion/ej2-angular-pdfviewer';
+
+@Component({
+  selector: 'app-root',
+  // specifies the template string for the PDF Viewer component
+  template: `<div class="content-wrapper">
+  <button (click)="save()">SaveDocument</button>
+  <button (click)="load_2()">LoadDocument</button>
+  <div class="content-wrapper">
+    <ejs-pdfviewer
+      id="pdfViewer"
+      [serviceUrl]="service"
+      [documentPath]="document"
+      style="height:640px;display:block"
+    ></ejs-pdfviewer>
+  </div>
+</div>`,
+  providers: [
+    PdfViewerComponent,
+    LinkAnnotationService, 
+    BookmarkViewService,
+    MagnificationService,
+    ThumbnailViewService, 
+    ToolbarService, 
+    NavigationService, 
+    TextSearchService, 
+    TextSelectionService, 
+    PrintService
+  ]
+})
+
+export class AppComponent implements OnInit {
+public service = 'https://localhost:44309/pdfviewer';
+public document = 'FormDesigner.pdf';
+ngOnInit(): void {
+}
+save() {
+  var viewer = (<any>document.getElementById('pdfViewer')).ej2_instances[0];
+  var data:Blob;
+  viewer.saveAsBlob().then(function(value:any) {
+    data = value;
+    var reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onload = () => {
+      base64data = reader.result;
+      console.log(base64data);
+      viewer.load(base64data, null);
+    };
+  });
+}
+load_2() {
+  // Load PDF document using file name
+  var viewer = (<any>document.getElementById('pdfViewer')).ej2_instances[0];
+  viewer.load(base64data, null);
+}
+}
+var base64data: string | ArrayBuffer|null;
