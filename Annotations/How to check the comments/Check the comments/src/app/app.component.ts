f6964f2f553e64103e67d44b625c0157f53e051a
RanjithaAmirthalingam <63278452+RanjithaAmirthalingam@users.noreply.github.com>
2022-10-19 13:43:09 +0530

    BLAZ-25694 Added the Check comments sample.

---
+++ Annotations/How to check the comments/Check the comments/src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import {
+  PdfViewerComponent,
+  LinkAnnotationService,
+  BookmarkViewService,
+  MagnificationService,
+  ThumbnailViewService,
+  ToolbarService,
+  NavigationService,
+  TextSearchService,
+  TextSelectionService,
+  PrintService,
+  AnnotationService,
+  FormFieldsService,
+} from '@syncfusion/ej2-angular-pdfviewer';
+
+/**
+ * Default PdfViewer Controller
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  encapsulation: ViewEncapsulation.None,
+  providers: [
+    LinkAnnotationService,
+    BookmarkViewService,
+    MagnificationService,
+    ThumbnailViewService,
+    ToolbarService,
+    NavigationService,
+    TextSearchService,
+    TextSelectionService,
+    PrintService,
+    AnnotationService,
+    FormFieldsService,
+  ],
+})
+export class AppComponent {
+  public service: string =
+    'https://localhost:5001/pdfviewer';
+  public document: string = 'PDF_Succinctly_Comments.pdf';
+  ngOnInit(): void {
+  }
+  //Method to check the comments added in the PDF document.
+  CheckComments(){
+    var pdfviewer = (<any>document.getElementById("pdfViewer")).ej2_instances[0];
+    var annotationCollections= pdfviewer.annotationCollection;
+    for(var x=0;x<annotationCollections.length;x++){
+        console.log(annotationCollections[x].annotationId);
+        var comments = annotationCollections[x].comments;
+        for(var y=0;y<comments.length;y++){
+            var comment = comments[y]; 
+            console.log("comment"+ "[" + y + "] :"+ comment.note);
+        }
+        var note = annotationCollections[x].note;
+        console.log("note : " + note);
+    }
+}
+}
