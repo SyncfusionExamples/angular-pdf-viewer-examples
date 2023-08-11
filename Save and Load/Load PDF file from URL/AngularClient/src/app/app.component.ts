655b4a462b2c1cb06214117eafc1d85271ec532a
SaravanaPriya31 <Saravana31>
2023-08-11 11:31:29 +0530

    842465 : Load the pdf form URL

---
+++ Save and Load/Load PDF file from URL/AngularClient/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { LinkAnnotationService, BookmarkViewService, MagnificationService,
+  ThumbnailViewService, ToolbarService, NavigationService,
+  TextSearchService, AnnotationService, TextSelectionService,
+  PrintService, FormDesignerService, FormFieldsService} from '@syncfusion/ej2-angular-pdfviewer';
+
+@Component({
+  selector: 'app-root',
+  // specifies the template string for the PDF Viewer component
+  template: 
+    `<div class="content-wrapper">
+                <ejs-pdfviewer id="pdfViewer"
+                       [serviceUrl]='service'
+                       [documentPath]='document'
+                       style="height:640px;display:block">
+                </ejs-pdfviewer>
+             </div>`,
+
+  providers: [ LinkAnnotationService, BookmarkViewService, MagnificationService,ThumbnailViewService, 
+               ToolbarService, NavigationService, AnnotationService, TextSearchService,
+                TextSelectionService, PrintService, FormDesignerService, FormFieldsService]
+  })
+export class AppComponent implements OnInit {
+  // Replace the `localhost:44396 with the actual URL of your server
+  public service = 'https://localhost:44396/pdfviewer';
+  // Replace correct PDF Document URL want to load
+  document="https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf"
+
+  ngOnInit(): void {
+  }
+}
