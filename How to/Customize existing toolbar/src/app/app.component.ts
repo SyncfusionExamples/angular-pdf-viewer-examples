57d87b2ddfa9d48e27906b97cd173e3530aea951
SaravanaPriya31 <Saravana31>
2023-12-18 18:37:19 +0530

    commit

--- How to/Customize existing toolbar/src/app/app.component.ts
+++ How to/Customize existing toolbar/src/app/app.component.ts
@@ -1,38 +1,54 @@
-
 import { Component, OnInit } from '@angular/core';
-import { LinkAnnotationService, BookmarkViewService, MagnificationService,
-         ThumbnailViewService, ToolbarService, NavigationService,
-         AnnotationService, TextSearchService, TextSelectionService,
-         PrintService, FormDesignerService, FormFieldsService, CustomToolbarItemModel
-       } from '@syncfusion/ej2-angular-pdfviewer';
+import {
+  LinkAnnotationService, BookmarkViewService, MagnificationService,
+  ThumbnailViewService, ToolbarService, NavigationService,
+  AnnotationService, TextSearchService, TextSelectionService,
+  PrintService, FormDesignerService, FormFieldsService, CustomToolbarItemModel
+} from '@syncfusion/ej2-angular-pdfviewer';
 
 @Component({
   selector: 'app-root',
-  // specifies the template string for the PDF Viewer component
   template: `<div class="content-wrapper">
                 <ejs-pdfviewer id="pdfViewer"
-                      [documentPath]='document'
-                      [toolbarSettings]='toolbarSettings'
+                      [documentPath]="document"
+                      [resourceUrl]="resource"
+                      [toolbarSettings]="toolbarSettings"
+                      (toolbarClick)="toolbarClick($event)"
                       style="height:640px;display:block">
                 </ejs-pdfviewer>
              </div>`,
-  providers: [ LinkAnnotationService, BookmarkViewService, MagnificationService,
-               ThumbnailViewService, ToolbarService, NavigationService,
-               AnnotationService, TextSearchService, TextSelectionService,
-               PrintService, FormDesignerService, FormFieldsService]
+  providers: [LinkAnnotationService, BookmarkViewService, MagnificationService,
+    ThumbnailViewService, ToolbarService, NavigationService,
+    AnnotationService, TextSearchService, TextSelectionService,
+    PrintService, FormDesignerService, FormFieldsService]
 })
 export class AppComponent implements OnInit {
   public document = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
-  // To utilize the server-backed PDF Viewer, need to specify the service URL. This can be done by including the **[serviceUrl]='service'** attribute within the <ejs-pdfviewer></ejs-pdfviewer> component in app.component.html file.
-  //public service: string ='https://services.syncfusion.com/angular/production/api/pdfviewer';
-  public toolItem: CustomToolbarItemModel = {
-        prefixIcon: "e-de-ctnr-lock",
-        tooltipText: "Disable Image",
-        text: "Disable Image",
-        id: "Custom"
-  };
-  public toolbarSettings = { toolbarItems: [this.toolItem, 'OpenOption', 'PageNavigationTool', 'MagnificationTool', 'PanTool', 'SelectionTool', 'SearchOption', 'PrintOption', 'DownloadOption', 'UndoRedoTool', 'AnnotationEditTool', 'FormDesignerEditTool', 'CommentTool', 'SubmitForm']}
+  public resource = 'https://cdn.syncfusion.com/ej2/24.1.41/dist/ej2-pdfviewer-lib';
 
+  public toolItem: CustomToolbarItemModel = {
+      prefixIcon: 'e-icons e-paste',
+      id: 'print',
+      tooltipText: 'Custom toolbar item',
+      align: 'left'
+  };
+
+  public toolbarSettings = {
+    showTooltip: true,
+    toolbarItems: [this.toolItem,'OpenOption', 'PageNavigationTool', 'MagnificationTool', 'PanTool', 'SelectionTool',
+      'SearchOption', 'PrintOption', 'DownloadOption', 'UndoRedoTool', 'AnnotationEditTool',
+      'FormDesignerEditTool', 'CommentTool', 'SubmitForm', this.toolItem]
+  };
+  public toolbarClick(args: any): void {
+      var pdfViewer = (<any>document.getElementById('pdfViewer')).ej2_instances[0];
+      if (args.item && args.item.id === 'print') {
+            pdfViewer.printModule.print();
+      } else if (args.item && args.item.id === 'download') {
+            pdfViewer.download();
+      }
+    }
   ngOnInit(): void {
   }
-}
+
+
+  }
