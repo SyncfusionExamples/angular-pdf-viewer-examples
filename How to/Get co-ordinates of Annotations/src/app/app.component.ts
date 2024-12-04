6404d8740ea6992ffddfe7f2efa07bca96c31803
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2024-12-04 15:26:57 +0530

    834567: How to get co-ordinates of Annotations

---
+++ How to/Get co-ordinates of Annotations/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  LinkAnnotationService,
+  BookmarkViewService,
+  MagnificationService,
+  ThumbnailViewService,
+  ToolbarService,
+  NavigationService,
+  AnnotationService,
+  TextSearchService,
+  TextSelectionService,
+  FormFieldsService,
+  FormDesignerService,
+  PrintService
+} from '@syncfusion/ej2-angular-pdfviewer';
+
+@Component({
+  selector: 'app-root',
+  // specifies the template string for the PDF Viewer component
+  template: `<div class="content-wrapper">
+  <ejs-pdfviewer id="pdfViewer"
+             [documentPath]='document'
+                 [resourceUrl]='resource' 
+             style="height:640px;display:block"
+             (annotationAdd)="onAnnotationAdd($event)">
+  </ejs-pdfviewer>
+</div>`,
+  providers: [
+    LinkAnnotationService,
+    BookmarkViewService,
+    MagnificationService,
+    ThumbnailViewService,
+    ToolbarService,
+    NavigationService,
+    AnnotationService,
+    TextSearchService,
+    TextSelectionService,
+    FormFieldsService,
+    FormDesignerService,
+    PrintService]
+})
+export class AppComponent implements OnInit {
+  public document = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+  public resource: string = 'https://cdn.syncfusion.com/ej2/27.2.2/dist/ej2-pdfviewer-lib';;
+  ngOnInit(): void {
+  }
+  onAnnotationAdd(args: any) {
+    console.log(args);  // Log the event arguments
+  }
+
+}
