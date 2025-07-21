3533b62516c8012860d1eeb4964c09077520c5f1
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-07-21 15:41:20 +0530

    932962: Updated Sample on How to Implement login page to control access

---
+++ How to/Implement login page to control access/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {
+  PdfViewerModule,
+  LinkAnnotationService,
+  BookmarkViewService,
+  MagnificationService,
+  ThumbnailViewService,
+  ToolbarService,
+  NavigationService,
+  TextSearchService,
+  TextSelectionService,
+  PrintService,
+  AnnotationService,
+  FormFieldsService,
+  FormDesignerService,
+  PageOrganizerService
+} from '@syncfusion/ej2-angular-pdfviewer';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    PdfViewerModule,
+    DialogModule
+  ],
+  providers: [
+    LinkAnnotationService,
+    BookmarkViewService,
+    MagnificationService,
+    ThumbnailViewService,
+    ToolbarService,
+    NavigationService,
+    TextSearchService,
+    TextSelectionService,
+    PrintService,
+    AnnotationService,
+    FormFieldsService,
+    FormDesignerService,
+    PageOrganizerService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
