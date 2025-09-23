db1a88b01741e0b0b23e2c2afb603e8da93cab7d
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-09-23 16:26:03 +0530

    983153: Sample on How to Detect Changes in PDF Viewer Using Annotation and Form Field Events

---
+++ How to/Detect Changes in PDF Viewer Using Annotation and Form Field Events/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewEncapsulation, OnInit,ViewChild} from '@angular/core';
+import { PdfViewerComponent, LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService, ToolbarService, NavigationService, TextSearchService, TextSelectionService, PrintService, AnnotationService, FormFieldsService, FormDesignerService, PageOrganizerService,PdfViewerModule, TextSelectionStartEventArgs, AnnotationSelectEventArgs } from '@syncfusion/ej2-angular-pdfviewer';
+import { SwitchComponent, SwitchModule } from '@syncfusion/ej2-angular-buttons';
+import { ClickEventArgs } from '@syncfusion/ej2-buttons';
+
+
+
+/**
+ * Default PdfViewer Controller
+ */
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    encapsulation: ViewEncapsulation.None,
+    // tslint:disable-next-line:max-line-length
+    providers: [LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService, ToolbarService, NavigationService,
+        TextSearchService, TextSelectionService, PrintService, AnnotationService, FormFieldsService, FormDesignerService,PageOrganizerService],
+    styleUrls: ['app.component.css'],
+    standalone: true,
+    imports: [
+        
+        SwitchModule,
+        PdfViewerModule,
+        
+    ],
+})
+
+export class AppComponent {
+    @ViewChild('pdfviewer')
+    public pdfviewerControl: PdfViewerComponent | undefined;
+    
+    public document: string ='https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+    public resource:string = "https://cdn.syncfusion.com/ej2/31.1.17/dist/ej2-pdfviewer-lib";
+    ngOnInit(): void {
+        // ngOnInit function
+    }
+    public triggerEvent(e: any): void {
+    console.log('Changes are observed in PDF Viewer!');
+  }
+}
